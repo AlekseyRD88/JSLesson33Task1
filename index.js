0a8759a8aea2149de0b69ba80cb9c01ee0e8ae92
= <neymegen50@gmail.com>
2022-05-24 22:56:25 +0300

    corrections

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
 
-const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
+const defaultAvatar = 'https://avatars.githubusercontent.com/u/1342004?v=4';
 userAvatarElem.src = defaultAvatar;
 
 const fetchUserData = (userName) => {
-  return fetch(`https://api.github.com/users/${userName}`).then((response) => response.json());
+  return fetch(`https://api.github.com/users/${userName}`)
+    .then((response) => response.json())
+    .then((result) => console.log(result));
 };
 
 const renderUserData = (userData) => {
+  console.log(userData);
   const { avatar__url, name, location } = userData;
   userAvatarElem.src = avatar__url;
   userNameElem.textContent = name;
