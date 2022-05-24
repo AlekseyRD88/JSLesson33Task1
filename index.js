35d74535d72291115cffe77189783e2f48e9821a
= <neymegen50@gmail.com>
2022-05-24 23:11:53 +0300

    message

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
 
-const defaultAvatar = 'https://avatars.githubusercontent.com/u/1342004?v=4';
+const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 userAvatarElem.src = defaultAvatar;
 
 const fetchUserData = (userName) => {
-  return fetch(`https://api.github.com/users/${userName}`)
-    .then((response) => response.json())
-    .then((result) => console.log(result));
+  return fetch(`https://api.github.com/users/${userName}`).then((response) => response.json());
 };
 
 const renderUserData = (userData) => {
-  console.log(userData);
-  const { avatar__url, name, location } = userData;
-  userAvatarElem.src = avatar__url;
+  //console.log(userData);
+  const { avatar_url, name, location } = userData;
+  userAvatarElem.src = avatar_url;
   userNameElem.textContent = name;
   userLocationElem.textContent = location ? `from ${location}` : '';
 };
